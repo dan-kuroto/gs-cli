f964181420f3301515947c722dd6eccabf0e95e4
dan-kuroto <2261268657@qq.com>
2023-12-08 07:19:18 +0800

    diff: 修改根命令、创建create命令

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -10,18 +9,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "gs-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
+	Use:     "gs-cli",
+	Version: "v1.1.0 Ginga Strium",
+	Short:   "gin-stronger CLI tool to generate application.",
+	Long: `gin-stronger is a library that empowers Gin.
 This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+to quickly create a gin-stronger application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -45,7 +40,5 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
